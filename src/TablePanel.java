2d6d66b4936ccc3c3e378446db9c052949c89477
kadownie <kadownie@gmail.com>
2017-12-05 14:46:25 -0500

    1.1

--- src/TablePanel.java
+++ src/TablePanel.java
@@ -2,9 +2,15 @@ import java.awt.BorderLayout;
 
 import javax.swing.JPanel;
 import javax.swing.JTable;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
 import javax.swing.table.DefaultTableModel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
-public class TablePanel extends JPanel {
+public class TablePanel extends JPanel implements TableModelListener{
 
 	private JTable table;
 	/**
@@ -24,20 +30,37 @@ public class TablePanel extends JPanel {
 		
 		Object[][] data = new Object[units][];
 		for(int i = 0; i < units; i++) {
-			data[i] = new Object[]{i+1, "", price, "", wages, ""};
+			data[i] = new Object[]{i+1, null, price, null, wages, null};
 		}
 		table = new JTable(data, columnNames);
 		
+		
 		DefaultTableModel tableModel = new DefaultTableModel(data, columnNames) {
+			Class<?>[] types = new Class[] {
+					java.lang.Integer.class, java.lang.Double.class, java.lang.Double.class, 
+					java.lang.Double.class, java.lang.Double.class, java.lang.Double.class
+			};
+			
+			@Override
+			public Class<?> getColumnClass(int columnIndex) {
+		        return types [columnIndex];
+		    }
+			
 			@Override
 			public boolean isCellEditable(int row, int column) {
 				return column == 1;
 			}
 		};
 		table.setModel(tableModel);
+		table.getModel().addTableModelListener(this);
 		setLayout(new BorderLayout());
 		add(table.getTableHeader(), BorderLayout.PAGE_START);
 		add(table, BorderLayout.CENTER);
+	}
+	
+	public void tableChanged(TableModelEvent e) {
+		int row = e.getFirstRow();
+		System.out.println(row);
 	}
 
 }
